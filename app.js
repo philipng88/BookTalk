b14127a9d681da0a4227907aa0986619a5ec1c71
philipng88 <philiplyng@gmail.com>
2019-08-01 16:14:31 -0600

    show/hide navbar content depending on login status

--- app.js
+++ app.js
@@ -30,6 +30,11 @@ passport.use(new LocalStrategy(User.authenticate()))
 passport.serializeUser(User.serializeUser())
 passport.deserializeUser(User.deserializeUser())
 
+app.use((req, res, next) => {
+    res.locals.currentUser = req.user 
+    next() 
+})
+
 app.get("/", (req, res) => {
     res.render("landing")
 })
